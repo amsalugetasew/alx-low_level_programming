cd592f1519eadfa2033e0c39af3c2af45886b42c
Getasew <42133391+amsalugetasew@users.noreply.github.com>
2023-03-28 02:31:33 -0700

    Add files via upload

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_puts_recursion(s-1);
+_putchar(s);
+}
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,7 @@
+#include<string.h>
+#include<conio.h>
+#include <unistd.h>
+void _putchar(char *x)
+{
+write(1, x, 1);
+}
