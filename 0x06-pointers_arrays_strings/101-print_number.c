41d8a8a1aaa78805e9f1d290b9e8f85b855490ee
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-29 02:25:04 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,14 +1,25 @@
-#include<stdio.h>
-#include<conio.h>
-#include"main.h"
+#include "main.h"
+
+/**
+ * print_number - Prints last digit of a number
+ * @n: - integer parameter
+ * Return: Always (Success)
+ *
+ */
+
 void print_number(int n)
 {
-if (n=='\n')
-{
-_putchar(1);
-}
-else
-{
-_putchar(n);
-}
+	unsigned int num = n;
+	if (n < 0)
+	{
+		n *= -1;
+		num = n;
+		_putchar('-');
+	}
+	num /= 10;
+	if (num != 0)
+	{
+		print_number(num);
+	}
+	_putchar((unsigned int) n % 10 + '0');
 }
