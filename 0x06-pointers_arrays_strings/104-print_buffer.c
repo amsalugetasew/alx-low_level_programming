38dde6a3dddb515023e7404f05a43bff2665a386
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-29 03:58:47 -0700

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,10 +1,52 @@
 #include<stdio.h>
-#include <stdlib.h>
+
+/**
+ * print_buffer - Prints buffer 
+ * @b: Character parameter 
+ * @size: - integer parameter
+ * Return: Always (Success)
+ *
+ */
+
 void print_buffer(char *b, int size)
 {
-printf(b);
-printf("%d",size);
-sprintf(b, "%d", size);
-snprintf( b, 256, "%d", 132 );
-printf( b, 256, "%d", 132 );
+	int i, j, k, d;
+	i = 0;
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (i < size)
+	{
+		j = size - i < 10 ? size - i : 10;
+		printf("%08x: ", i);
+		for ( k = 0; k < 10; k++)
+		{
+			if (k < j)
+			{
+				printf("%02x", *(b + i +k));
+			}
+			else
+			{
+				printf(" ");
+			}
+			if (k % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (k = 0; k < j; k++)
+		{
+			d = *(b + i + k);
+			if (d < 32 || d > 132)
+			{
+				d = '.';
+				printf("%c", d);
+			}
+			printf("\n");
+			i += 10;
+		}
+	}
 }
+
