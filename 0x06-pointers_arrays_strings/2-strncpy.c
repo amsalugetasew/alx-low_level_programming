dfa66dc1396f4a1a9b79b949fa022f6696ebcaea
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-29 04:31:17 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,14 @@
 #include<string.h>
+
+/**
+ * *_strncpy - Prints n copy string
+ * @dest: first charcter string parameter
+ * @src: second character string parameter
+ * @n: third integer parameter
+ * Return: Always (Success)
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-return (strncpy(dest, src, n));
+	return (strncpy(dest, src, n));
 }
