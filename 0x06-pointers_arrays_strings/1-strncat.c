c0bbc282d091c65bb0a539bdb57c1a753aaa31b9
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-28 22:00:24 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,12 @@
 #include<string.h>
+/**
+ * *_strncat - Prints n number of concatinated values of string
+ * @dest: first charcter string parameter
+ * @src: second character string parameter
+ * @n: third integer parameter
+ * Return: Always (Success)
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-return (strncat(dest, src, n));
+	return (strncat(dest, src, n));
 }
