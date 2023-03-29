1e746e9eb14aa19c05380419b1dfe19bc2f53211
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-28 23:25:18 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,25 @@
-#include<stdio.h>
 #include<string.h>
-#include<ctype.h>
+
+/**
+ * *cap_string -  capitalizes all words of a string
+ * @str: charcter string parameter
+ * Return: Always (Success)
+ */
+
 char *cap_string(char * str)
 {
-int i,len=strlen(str);
-for(i = 0; i < len;i++)
-{
-if (*(str+i)==33 && *(str+i+1)>96)
-{
-*(str+i+1)-=32;
-}
-if(*(str+i)<97)
-{
-*(str+i)-=0;
-}
-}
-return str;
+	int i, len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (((*(str + i) == 32) || (*(str + i) == 10) || (*(str + i) == 9) || (*(str + i) == 46)) && *(str + i + 1) > 96)
+		{
+			*(str + i + 1) -= 32;
+		}
+		else
+		{
+			*(str + i) -= 0;
+		}
+	}
+	return str;
 }
