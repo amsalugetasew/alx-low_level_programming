c0359fe42c24d052cd95a85d614d62056bedce3b
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-28 22:15:18 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,13 @@
 #include<string.h>
+
+/**
+ * *_strcmp - Prints +ve number if s1>s2, -ve if s1<s2, and 0 otherwise
+ * @s1: first charcter string parameter
+ * @s2: second character string parameter
+ * Return: Always (Success)
+ */
+
 int _strcmp(char *s1, char *s2)
 {
-return (strcmp(s1, s2));
+	return (strcmp(s1, s2));
 }
