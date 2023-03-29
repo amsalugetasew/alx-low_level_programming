4f7c54229c1a54fedbf6d548433c83975558b997
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-28 22:44:54 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
+
+/**
+ * *_strncat - Prints reverses the content of an array of integers
+ * @a: first integer  parameter
+ * @n: second integer parameter
+ * Return: Always (Success)
+ */
+
 void reverse_array(int *a, int n)
 {
-int i = n;
-for(i=n-1;i>=0;i--)
-{
-if (i != n-1)
-{
-printf(", ");
-}
-printf("%d",*(a+i));
-}
-printf("\n");
+	int i = n;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+		printf("%d",*(a+i));
+	}
+	printf("\n");
 }
