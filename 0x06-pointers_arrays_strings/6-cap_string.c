5281053d47be7b7afb21b01da842b4d435a1b566
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-30 01:52:28 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,12 @@ char *cap_string(char * str)
 
 	for (i = 0; i < len; i++)
 	{
-		if (((*(str + i) == 32) || (*(str + i) == 10) || (*(str + i) == 9) || (*(str + i) == 46)) && *(str + i + 1) > 96)
+		if (((*(str + i) == 32) || (*(str + i) == 10) || (*(str + i) == 9) || (*(str + i) == 46)))
 		{
-			*(str + i + 1) -= 32;
+			if( *(str + i + 1) > 96)
+			{
+				*(str + i + 1) -= 32;
+			}
 		}
 		else
 		{
