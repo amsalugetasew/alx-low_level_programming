a219b891c7493e75bef7004a1da81ac4b45a2359
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-31 01:58:47 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
-
 /**
  * reverse_array - Prints reverses the content of an array of integers
  * @a: first integer  parameter
  * @n: second integer parameter
  * Return: Always (Success)
  */
-
 void reverse_array(int *a, int n)
 {
 	int i, temp;
@@ -14,7 +12,7 @@ void reverse_array(int *a, int n)
 	for (i = n - 1; i >= n / 2; i--)
 	{
 		temp = a[n - 1 - i];
-		a[n - 1 -i] = a[i];
+		a[n - 1 - i] = a[i];
 		a[i] = temp;
 	}
 }
