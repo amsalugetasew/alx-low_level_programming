07eba57fec37dd5192846148b9416dd666b74d87
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-30 22:29:16 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,11 @@
 #include<string.h>
 
 /**
- * *_strcat - Prints the concatinated value of two strings 
+ * *_strcat - Prints the concatinated value of two strings
  * @dest: first character string parameter
  * @src: second character string parameter
  * Return: Always (Success)
  */
-
 char *_strcat(char *dest, char *src)
 {
 	dest = strcat(dest, src);
