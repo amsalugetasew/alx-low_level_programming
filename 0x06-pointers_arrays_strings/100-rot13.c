ad6ded56cbc8fc1be9d58c593a23e66b12c8c68e
amsalugetasew <amsalugetasew12@gmail.com>
2023-03-31 01:16:50 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,20 +7,20 @@
  */
 char *rot13(char *s)
 {
-        int i, j;
-        char ch1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        char ch2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+	char ch1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char ch2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                for (j = 0; j < 52; j++)
-                {
-                        if (s[i] == ch1[j])
-                        {
-                                s[i] = ch2[j];
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == ch1[j])
+			{
+				s[i] = ch2[j];
 				break;
-                        }
-                }
-        }
-        return (s);
+			}
+		}
+	}
+	return (s);
 }
