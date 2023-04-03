2a601ca750ab4a8097a430d4f537321eb9ef03df
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-03 00:48:27 -0700

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,14 @@
-#include<string.h>
+#include <string.h>
+
+/**
+ * *_strpbrk - Prints the scanned string
+ * @s: first charcter string parameter
+ * @accept: second character string parameter
+ * Return: Always (Success)
+ */
+
 char *_strpbrk(char *s, char *accept)
 {
-char *match;
-match = strpbrk(s, accept);
-return (match);
+	s = strpbrk(s, accept);
+	return (s);
 }
