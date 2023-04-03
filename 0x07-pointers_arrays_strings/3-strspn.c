e36a138014b4d80255aa8559eee1f0a40c24e5eb
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-03 00:35:16 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,16 @@
 #include <string.h>
+
+/**
+ * *_strspn - Prints a copies of memory area
+ * @s: first charcter string parameter
+ * @accept: second character string parameter
+ * Return: Always (Success)
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
- int len = strspn( s,accept );
- return (len);
+	unsigned int num_of_match;
+	num_of_match = strspn(s, accept);
+
+	return (num_of_match);
 }
