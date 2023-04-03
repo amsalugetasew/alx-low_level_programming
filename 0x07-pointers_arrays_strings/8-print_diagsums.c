7df4a4043503330b933080fb0cdd6dbb15b2a222
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-03 03:18:32 -0700

    Task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,47 @@
 #include<stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - Prints the sum of the two diagonas with squer array
+ * @a: - first integer parameter
+ * @size: second integre parameter
+ * Return: Always (Success)
+ */
+
+
 void print_diagsums(int *a, int size)
 {
-int i,j,diagonal, *p;
-for(i = 0;i < size; i++)
-{
-diagonal = 0;
-for(j = 0;j < size; j++)
-{
-if(i == j)
-{
-p = &a[i];
-/*printf("%d", *(p+i));
-printf(" %d ", i);*/
-diagonal = diagonal + *(p + j);
-printf("%d", diagonal);
-}
-}
-printf("\n");
-}
-
-
-
-
+	int i, j, diagonal[2], num, num1;
+	num = 0;
+	num1 = 0;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				num += *(a + i * size + j);
+			}
+			if (i == (size - 1) - j)
+			{
+				num1 += *(a + i * size + j);
+			}
+		}
+	}
+	diagonal[0] = num;
+        diagonal[1] = num1;
+	for ( i = 0; i <= 1; i++)
+	{
+		if ( i == 0)
+		{
+			printf("%d",(diagonal[i]));
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d",(diagonal[i]));
+		}
+	}
+	printf("\n");
 }
