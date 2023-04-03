10bc6a89c6d80ce1134fdbd4dfb1d1216680c355
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-02 23:12:16 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,20 @@
 #include<string.h>
+/**
+ * *_memset - Prints a pointer of memory area
+ * @s: first charcter string parameter
+ * @b: second character string parameter
+ * @n: third unsigned integer parameter
+ * Return: Always (Success)
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i; 
-for(i = 0; i < n; i++) 
-{
-s = &b;
-s = memset(s + 13, '.', 8*sizeof(char));
-}
-return (s);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s = &b;
+		s = memset(s + 13, '.', 8*sizeof(char));
+	}
+	return (s);
 }
