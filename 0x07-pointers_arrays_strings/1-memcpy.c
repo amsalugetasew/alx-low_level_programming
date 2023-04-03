fdc5d3eb3df67afef96ab20615fc9b8b24531b1d
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-03 00:07:47 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,16 @@
 #include <string.h>
-#include<stdio.h>
+
+/**
+ * *_memcpy - Prints a copies of memory area
+ * @dest: first charcter string parameter
+ * @src: second character string parameter
+ * @n: third unsigned integer parameter
+ * Return: Always (Success)
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-strncpy(dest,src,n);
-return (dest);
+	memcpy(dest, src, n);
+
+	return (dest);
 }
