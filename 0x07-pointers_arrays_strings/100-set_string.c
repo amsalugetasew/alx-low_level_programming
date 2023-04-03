86028b6c43f4eab3174ea22acaa0e63ac04b66a2
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-03 03:31:06 -0700

    Task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+/**
+ * print_to_98 - Prints all numbers
+ * @n: - first integer parameter
+ * Return: Always (Success)
+ *
+ */
+
 void set_string(char **s, char *to)
 {
-*s = to;
+	*s = to;
 }
