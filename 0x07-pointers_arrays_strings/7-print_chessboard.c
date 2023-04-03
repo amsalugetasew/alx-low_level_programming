b7f67636637a219d2f0c78586ccaf63c9a773d51
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-03 03:51:38 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 
 /**
  * print_chessboard - Prints the chessboard
- * @a: array
+ * @a: Array
  * Return: Always (Success)
  */
 
@@ -14,7 +14,7 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(*( *(a + i) + j) );
+			_putchar(*(*(a + i) + j));
 		}
 		_putchar('\n');
 	}
