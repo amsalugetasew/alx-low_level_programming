ca8c67a04d45f2afd5699daf316a59f457cc86dc
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-05 08:17:32 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - Prints length string using recursion
+ * @s: - first character parameter
+ * Return: Always (Success)
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
