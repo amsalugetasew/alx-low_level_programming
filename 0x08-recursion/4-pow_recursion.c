34f1611725158cd530aace69f4c821d8ae56599a
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-05 10:13:19 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+ * _pow_recursion - Prints power of a number recursion
+ * @x: firts integer paramenter
+ * @s: - first integer parameter
+ * Return: Always (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else 
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
