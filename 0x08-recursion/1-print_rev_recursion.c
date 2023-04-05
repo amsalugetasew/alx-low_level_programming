778d17ee28caebc121ebf88cfc2ca5917101fe57
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-05 08:06:55 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,16 @@
-#include"main.h"
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - Prints string using
+ * @s: - first character parameter
+ * Return: Always (Success)
+ */
 void _print_rev_recursion(char *s)
 {
-if (*s)
-{
-_print_rev_recursion(s + 1);
-_putchar(s);
-}
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion( s + 1);
+	putchar(*s);
 }
