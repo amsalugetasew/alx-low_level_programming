da12fde3b32a7e1f29f1ee6f598f09fad956147b
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-05 07:40:45 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,9 @@
  */
 void _puts_recursion(char *s)
 {
-        if (*s)
-        {
-                _puts_recursion(s - 1);
-                printf("%s\n", s);
-        }
+	if (*s)
+	{
+		_puts_recursion(s - 1);
+		printf("%s\n", s);
+	}
 }
