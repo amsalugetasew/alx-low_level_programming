07a24c9dde44106a8a862fba6803faf53640a790
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-04 22:13:35 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,16 @@
 #include "main.h"
+
+/**
+ * _puts_recursion - Prints string using 
+ * @s: - first character parameter
+ * Return: Always (Success)
+ */
+
 void _puts_recursion(char *s)
 {
-if (*s)
-{
-_puts_recursion(s-1);
-_putchar(s);
-}
+	if (*s)
+	{
+		_puts_recursion(s-1);
+		_putchar(s);
+	}
 }
