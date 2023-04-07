4fa742fbf1a33c09d0ce3c90cc511f0d38679d8f
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-07 05:13:43 -0700

    Task 0

---
+++ 0x09-static_libraries/14-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        int i = 0;
+        char str[] = "Look up!\n";
+        char ptr;
+
+        while (str[i])
+        {
+                ptr = str[i];
+                printf("%c", _islower(ptr));
+                i++;
+        }
+        return (0);
+}
