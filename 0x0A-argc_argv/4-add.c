87e158cfa9405dbc06fef92f79b791ffc7a499c5
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-07 00:22:42 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Prints addition of argument value
+ * @argc: firts input
+ * @argv: second input
+ * Return: Always (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, counter = 0, add = 0;
+	char *next;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			add += strtol (argv[i], &next, 10);
+			if ((next == argv[i]) || (*next != '\0'))
+			{
+				counter++;
+			}
+		}
+		if(counter == 0)
+		{
+			printf("%d\n", add);
+		}
+		else
+		{
+			printf("%s\n", "Error");
+		}
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
+	return (0);
+}
