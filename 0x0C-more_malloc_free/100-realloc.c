2b91780655b09a23a8652655c855ca9c73766447
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-12 00:05:56 -0700

    Task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_realloc - Realocate memory block
+ * @ptr: first input
+ * @old_size: second input
+ * @new_size: third input
+ * Return: Always (Success)
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	unsigned int i;
+
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		return (p);
+	}
+	else
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+	}
+	p = malloc(new_size);;
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		p[i] = ((char *) ptr)[i];
+	}
+	free(ptr);
+	return (p);
+}
