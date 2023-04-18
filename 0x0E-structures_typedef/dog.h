d2ccfb94d84fea73b16fdaf4479abdd7d403e345
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-17 23:53:32 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - Structure declaration name dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: declartaion of new dog structure with three members of character float and character repectively
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
