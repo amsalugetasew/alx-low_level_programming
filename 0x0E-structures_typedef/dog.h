693fb4da063c4a4fd2bab512b6e10d2c643b846b
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-18 01:02:51 -0700

    Task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef _HEADER_
+#define _HEADER_
 /**
  * struct dog - Structure declaration name dog
  * @name: First member
@@ -11,3 +13,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
