3de8a11ef17daaf643b29fafd2622bd29ed52e2d
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-19 01:31:36 -0700

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * int_index - Check the element match & Print the index of an arrray
+ * @array: first parameter
+ * @size: second parm
+ * @cmp: function as parameter
+ * Return: Always (Sucess).
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) > 0)
+		{
+			return ((i));
+		}
+	}
+	return (i);
+}
