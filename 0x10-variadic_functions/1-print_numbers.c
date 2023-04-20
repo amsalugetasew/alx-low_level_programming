2869085808c7f9db9b06d4a480f83ec5012de388
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-20 00:54:42 -0700

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_numbers - Print numbers, followed by a new line
+ * @separator: first input
+ * @n: second input
+ * 
+ * Return: Always (Sucess).
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list_arg;
+	va_start(list_arg, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+		{
+			continue;
+		}
+		else if (i == n - 1)
+		{
+			printf("%d", va_arg(list_arg, int));
+		}
+		else
+		{
+			printf("%d, ", va_arg(list_arg, int));
+		}
+	}
+	printf("\n");
+}
