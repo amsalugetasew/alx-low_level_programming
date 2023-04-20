aafc9e9214113c8c7a30cfdc4100738258f6e43f
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-20 03:10:49 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,8 +11,10 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+
 	va_list list_arg;
 	va_start(list_arg, n);
+
 	for (i = 0; i < n; i++)
 	{
 		if (separator == NULL)
