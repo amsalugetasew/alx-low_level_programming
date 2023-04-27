7278e093662dec913e0a941f9dc491f604d43624
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-27 03:52:53 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,31 +3,25 @@
 /**
  * print_list - prints all the elements of a list
  * @h: first input
+ *
  * Return: Always(sucess)
  */
 size_t print_list(const list_t *h)
 {
 	size_t number = 0;
 
-	if (h->next == NULL)
+	while (h)
 	{
-		printf("[0] (nil)");
-	}
-	else
-	{
-		while (h)
+		if (h->str == NULL)
 		{
-			if (h->str == NULL)
-			{
-				printf("[0] (nil)\n");
-			}
-			else
-			{
-				printf("[%d] %s\n", h->len, h->str);
-			}
-			number++;
-			h = h->next;
+			printf("[0] (nil)\n");
 		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		number++;
+		h = h->next;
 	}
 	return (number);
 }
