5389619076db5924cc040a66584a4d69ec151f8c
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-27 05:26:39 -0700

    Commite Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * first_fun - Returns sentence 
+ * @void: void parameter
+ * Return: Always (0)
+ */
+void first_fun(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
