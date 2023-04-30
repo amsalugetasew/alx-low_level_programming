bf0e6f675726e8498c1b6b3b411f8a08e92eb4a2
amsalugetasew <amsalugetasew12@gmail.com>
2023-04-30 03:36:19 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * *insert_nodeint_at_index - inserts a new node at a given position
  * @head: first input
@@ -22,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	temp->n = n;
 	temp->next = NULL;
-	while (temp1 && i != (idx - 1))
+	while (temp1 && (i != (idx - 1)))
 	{
 		temp1 = temp1->next;
 		++i;
